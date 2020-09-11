a4482e37b5e569c579b9ecf99bd9c77e08803b3a
Chen Sicong <sicong.chen@163.com>
2020-09-11 22:11:15 +0800

    [ChenSicong]feat: solved problem2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,16 +1,31 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import javax.annotation.PostConstruct;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PersonVo {
 
   private String id;
   private Integer age;
   private String name;
   private String hobby;
+
+  public PersonVo(String id, Integer age, String name, String hobby) {
+    this.id = id;
+    this.age = age == null ? 0 : age;
+    this.name = name;
+    this.hobby = hobby;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age == null ? 0 : age;
+  }
 }
